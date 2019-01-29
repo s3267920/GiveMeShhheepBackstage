6282b25d1e86eac3009a49203156bc0778959920
Lucy <s3267920@gmail.com>
2019-01-29 21:04:23 +0800

    重構

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,23 @@
 <template>
   <div id="app">
+    <Home/>
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">首頁Home</router-link>|
+      <router-link to="/order">訂單Orders</router-link>|
+      <router-link to="/product">產品Product</router-link>
     </div>
-    <router-view/>
   </div>
 </template>
+<script>
+import Home from './components/homeComponents/Index.vue';
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+export default {
+  name: 'app',
+  data() {
+    return {};
+  },
+  components: {
+    Home,
+  },
+};
+</script>
---
+++ src/components/homeComponents/Index.vue
@@ -0,0 +1,8 @@
+<script>
+export default {
+  name: 'home',
+};
+</script>
+<template src="./index.html"></template>
+<style lang="scss" src="@/scss/layout.scss"></style>
+<style lang="scss" src="@/scss/index.scss"></style>
