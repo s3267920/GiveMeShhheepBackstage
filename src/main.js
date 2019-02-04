2dea78386b67ace1c68f4a14ea84ee1b6b6f37cd
Lucy <s3267920@gmail.com>
2019-02-04 20:33:01 +0800

    裝vue-fontawesome

--- src/main.js
+++ src/main.js
@@ -1,13 +1,30 @@
 import Vue from 'vue';
+import App from './App.vue'; // eslint-disable-line 
+import router from './router'; // eslint-disable-line 
+import store from './store'; // eslint-disable-line 
+
 import {
   sync,
 } from 'vuex-router-sync';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import {
+  library,
+} from '@fortawesome/fontawesome-svg-core';
+import {
+  faUser,
+  faBuilding,
+} from '@fortawesome/free-solid-svg-icons';
+import {
+  FontAwesomeIcon,
+} from '@fortawesome/vue-fontawesome';
+import {
+  faUserCircle,
+  faTimesCircle,
+} from '@fortawesome/free-regular-svg-icons';
 
 
+library.add(faUserCircle, faTimesCircle, faUser, faBuilding);
 sync(store, router);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 
 Vue.config.productionTip = false;
--- src/router.js
+++ src/router.js
@@ -3,28 +3,41 @@ import Router from 'vue-router';
 import Home from './components/homeComponents/Home.vue';
 import Orders from './components/ordersComponents/Orders.vue';
 import Product from './components/productComponents/Product.vue';
+import LogIn from './components/LogIn.vue';
+import Register from './components/Register.vue';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [{
-    path: '/',
+    path: '/backstage',
     name: 'home',
     component: Home,
   },
   {
-    path: '/orders',
+    path: '/login',
+    name: 'login',
+    components: LogIn,
+  },
+  {
+    path: '/register',
+    name: 'register',
+    components: Register,
+  },
+  {
+    path: '/backstage/orders',
     name: 'orders',
     component: Orders,
   },
   {
-    path: '/product',
+    path: '/backstage/product',
     name: 'product',
     component: Product,
   },
   {
     path: '/*',
-    redirect: '/',
+    redirect: '/backstage',
   },
   ],
 });
