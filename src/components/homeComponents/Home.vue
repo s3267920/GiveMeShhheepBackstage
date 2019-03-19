3bef6d48e4131e749235c64047ad9a886cac4599
Lucy <s3267920@gmail.com>
2019-03-19 21:53:53 +0800

    修改home html名稱並build

--- src/components/homeComponents/Home.vue
+++ src/components/homeComponents/Home.vue
@@ -1,4 +1,4 @@
-<template src="./index.html"></template>
+<template src="./home.html"></template>
 <style lang="scss" src="@/scss/layout.scss"></style>
 <style lang="scss" src="@/scss/index.scss"></style>
 <script>
